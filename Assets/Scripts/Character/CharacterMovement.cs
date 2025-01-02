d9ff282571a597919db2658afbf1fa37959325a3
ParavanoQuentin <93211239+ParavanoQuentin@users.noreply.github.com>
2025-01-02 01:26:48 +0100

    Animation déplacement

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -8,9 +8,11 @@ public class CharacterMovement : MonoBehaviour
     private PlayerInput inputs;
     private InputAction moveAction;
     private GameManager manager;
+    private Animator anim;
 
     private Vector2 velocity = Vector2.zero;
     [SerializeField] private float speed = 5f;
+    private int direction = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +20,7 @@ public class CharacterMovement : MonoBehaviour
         manager = GameManager.GetInstance();
         inputs = manager.GetInput();
         moveAction = inputs.actions.FindAction("Move");
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,6 +30,32 @@ public class CharacterMovement : MonoBehaviour
 
         velocity = moveValue * speed;
 
+        direction = AnimationDeplacement(velocity);
+
+        anim.SetInteger("direction", direction);
+
         transform.position += new Vector3(velocity.x * Time.fixedDeltaTime, velocity.y * Time.fixedDeltaTime, 0);
+    }
+
+    private int AnimationDeplacement(Vector2 velocity)
+    {
+        if (velocity.x < 0 && Mathf.Abs(velocity.x) >= Mathf.Abs(velocity.y))
+        {
+            return 4;
+        }
+        if (velocity.x > 0 && Mathf.Abs(velocity.x) >= Mathf.Abs(velocity.y))
+        {
+            return 6;
+        }
+        if (velocity.y < 0 && Mathf.Abs(velocity.x) < Mathf.Abs(velocity.y))
+        {
+            return 2;
+        }
+        if (velocity.y > 0 && Mathf.Abs(velocity.x) < Mathf.Abs(velocity.y))
+        {
+            return 8;
+        }
+
+        return 0;
     }
 }
